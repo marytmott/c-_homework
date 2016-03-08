8002689c88dc48dab81aee8510f1a6f3f38a4343
Mary Mott <mott.maryt@gmail.com>
2016-03-08 15:33:31 -0800

    part 1 complete

---
+++ date_when/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DateWhen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          // Console.WriteLine(DateTime.Today);
+          // DateTime date1 = new DateTime(2016, 1, 1);
+          // DateTime date2 = new DateTime(2017, 03, 5);
+
+          // bool isAfter1 = myDate.IsAfter(date1);  // true
+          // bool isAfter2 = myDate.IsAfter(date2);  // false
+
+          // Console.WriteLine(date2);
+          // Console.WriteLine(isAfter1);
+          // Console.WriteLine(isAfter2);
+
+
+          int dayth = myDate.DayInYear();
+          Console.WriteLine(dayth);
+        }
+    }
+
+    public class myDate
+    {
+      /// <summary>
+      /// Determine if this date d is before today's date
+      /// </summary>
+      /// <param name="d"></param>
+      /// <returns>true if it is, otherwise false</returns>
+      public static bool IsBefore(DateTime d)
+      {
+          // get today's date
+          DateTime today = DateTime.Today;
+
+          if (today > d) {
+            return true;
+          }
+
+          return false;
+      }
+      /// <summary>
+      /// Determine if this date d is after today's date
+      /// </summary>
+      /// <param name="d"></param>
+      /// <returns>true if it is, otherwise false</returns>
+      public static bool IsAfter(DateTime d)
+      {
+          // get today's date
+          DateTime today = DateTime.Today;
+
+          if (d > today) {
+            return true;
+          }
+
+          return false;
+      }
+      /// <summary>
+      /// Returns the number of today in the year
+      /// </summary>
+      /// <returns>returns a number 1...366, inclusive, such that this Date is the nth day of its year.</returns>
+      public static int DayInYear()
+      {
+          // get Jan 1 of this year
+          DateTime today = DateTime.Today;
+          DateTime jan1 = new DateTime(today.Year, 1, 1);
+
+          // subtract it from today
+          TimeSpan dayth = today - jan1;
+
+          return dayth.Days;
+
+      }
+    }
+}
---
+++ leap_year/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLeapYearRet1 = MyDate.IsLeapYear(2004);
+            bool isLeapYearRet2 = MyDate.IsLeapYear(2000);
+            bool isLeapYearRet3 = MyDate.IsLeapYear(100);
+            bool isLeapYearRet4 = MyDate.IsLeapYear(4);
+            Console.WriteLine(isLeapYearRet1);
+            Console.WriteLine(isLeapYearRet2);
+            Console.WriteLine(isLeapYearRet3);
+            Console.WriteLine(isLeapYearRet4);
+        }
+    }
+
+    public static class MyDate
+    {
+        // check whether leap year
+        // year to be verified
+        // true if leap year, otherwise false
+
+
+
+        public static bool IsLeapYear(int year)
+        {
+            // your implementation here
+            year = Convert.ToInt32(year);
+
+            // conditions for leap year
+            bool condition1 = year % 4 == 0;
+            bool condition2 = year % 100 != 0;
+            bool condition3 = year % 400 == 0;
+
+            if ((condition1 && condition2) || condition3)
+            {
+                Console.WriteLine("{0} is a leap year!", year);
+                return true;
+            }
+            return false;
+        }
+    }
+}

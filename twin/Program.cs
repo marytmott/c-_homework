7da79554bab1b8e60f5d09b44f231fc6e4f5daad
Mary Mott <mott.maryt@gmail.com>
2016-03-10 12:26:34 -0800

    twin completed

--- twin/Program.cs
+++ twin/Program.cs
@@ -2,14 +2,6 @@
 
 namespace ConsoleApplication
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-
     /// Write the twin() method so that it performs as indicated in the comment. Your solution should not use arrays.
     /// <summary>
     /// Use your definition of linkedlist in this method
@@ -34,7 +26,7 @@ namespace ConsoleApplication
             // Console.WriteLine(testNode);
             // Console.WriteLine(testNode2);
 
-            int[] nums = {0, 0, 0, 0, 1, 1, 0, 0, 0, 3, 3, 3, 1, 1, 0};
+            int[] nums = {3, 7, 4, 2, 2};
             MyLinkedList testList = new MyLinkedList();
 
             foreach(int num in nums)
@@ -42,22 +34,12 @@ namespace ConsoleApplication
                 testList.Add(num);
             }
 
-            MyLinkedList.squish(testList);
+            int origLength = testList.Length;
+            Console.WriteLine(origLength);
+
+            MyLinkedList.twin(testList);
         }
     }
-
-    /// <summary>
-    /// Create your own definition of linkedlist first
-    /// squish() takes your linked list and, wherever two or more consecutive items are
-    /// equals(), it removes duplicate nodes so that only one consecutive copy
-    /// remains. Hence, no two consecutive items in this list are equals() upon
-    /// completion of the procedure.
-    ///
-    /// After squish() executes, the list may well be shorter than when squish()
-    /// began. No extra items are added to make up for those removed.
-    /// For example, if the input list is [ 0 0 0 0 1 1 0 0 0 3 3 3 1 1 0 ], the
-    /// output list is [ 0 1 0 3 1 0 ].
-    /// </summary>
 
     public class MyLinkedList
     {
@@ -126,22 +108,86 @@ namespace ConsoleApplication
             return newNode;
         }
 
+        // makes ref nodes only
+        public void Double(Node node)
+        {
+
+            // new node
+            // Node copy1 = new Node();
+            // Node copy2 = new Node();
+
+            // Dictionary<string, object> copies = new Dictionary<string, object>;
+            // copies.Add("copy1", copy1);
+            // copies.Add("copy2", copy2)
+
+            // do it twice for every node
+            for (var j = 0; j < 2; j++)
+            {
+                // Console.WriteLine("copy {0}", j + 1);
+                Node copy = new Node();
+                copy.CopyOf = node;
+                this.length++;
+
+                // if there is head node
+                if (this.head != null)
+                {
+                    this.tail.Next = copy;
+                }
+                // set new head if none
+                else
+                {
+                    this.head = copy;
+                }
+                this.tail = copy;
+
+                // add to dictionary
+
+            }
+        }
+
+        // this should be a to string thang
+        // just prints out the vals of the linked list
         public static void forEach(MyLinkedList list)
         {
-            // to show squished list
+            // to show list
             Node currNodeEach = list.Head;
 
             for (int i = 0; i < list.Length; i++)
             {
+                int copyVal = currNodeEach.CopyOf.Num;
                 int nodeVal = currNodeEach.Num;
-                Console.WriteLine("Value at Node {0}: {1}", i, nodeVal);
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("{0} points to Node Value: {1}", i, copyVal);
+                Console.WriteLine("My value is: {0} (0 is false)", nodeVal);
                 currNodeEach = currNodeEach.Next;
             }
         }
 
-        public void twin(MyLinkedList list)
+        /// Write the twin() method so that it performs as indicated in the comment. Your solution should not use arrays.
+        /// <summary>
+        /// Use your definition of linkedlist in this method
+        /// twin() takes yourlinkedlist type and doubles its length by replacing each node
+        /// with two consecutive nodes referencing the same item.
+        /// For example, if the input list is [ 3 7 4 2 2 ], the
+        /// output list is [ 3 3 7 7 4 4 2 2 2 2 ].
+        /// IMPORTANT: Do not try to make new copies of the items themselves.
+        /// Make new nodes but just copy the reference to the items
+        /// </summary>
+        public static void twin(MyLinkedList list)
         {
             // Fill in your solution here.
+            MyLinkedList output = new MyLinkedList();
+
+            Node currNode = list.Head;
+
+            // go through the list
+            for (var i = 0; i < list.Length; i++)
+            {
+                output.Double(currNode);
+                currNode = currNode.Next;
+            }
+
+            MyLinkedList.forEach(output);
         }
     }
 
@@ -150,6 +196,7 @@ namespace ConsoleApplication
         // fields
         int num;
         Node next = null;
+        Node copyOf = null;
 
         // properties
         public int Num
@@ -175,6 +222,18 @@ namespace ConsoleApplication
                 next = value;
             }
         }
+
+        public Node CopyOf
+        {
+            get
+            {
+                return copyOf;
+            }
+            set
+            {
+                copyOf = value;
+            }
+        }
         // destructor -- TODO!
         // ~Node()
         // {
@@ -185,6 +244,11 @@ namespace ConsoleApplication
         public Node(int number)
         {
             num = number;
+        }
+
+        // OVERLoAD
+        public Node()
+        {
         }
     }
 }

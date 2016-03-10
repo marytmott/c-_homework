e0de4e1a89f07e62112e9487453a1bad3f0dc5a3
Mary Mott <mott.maryt@gmail.com>
2016-03-10 15:08:58 -0800

    removed junk comments from twin

--- twin/Program.cs
+++ twin/Program.cs
@@ -111,48 +111,6 @@ namespace ConsoleApplication
             return newNode;
         }
 
-        // makes ref nodes only
-        // public void Copies(Node node)
-        // {
-
-        //     // new node
-        //     Node copy1 = new Node();
-        //     Node copy2 = new Node();
-
-        //     Dictionary<string, object> copies = new Dictionary<string, object>;
-        //     copies.Add("copy1", copy1);
-        //     copies.Add("copy2", copy2
-
-
-        //         copy1.CopyOf = currNode;
-        //         copy2.CopyOf = currNode;
-
-        //     // do it twice for every node
-        //     for (var j = 0; j < 2; j++)
-        //     {
-        //         // Console.WriteLine("copy {0}", j + 1);
-        //         Node copy = new Node();
-        //         copy.CopyOf = node;
-
-
-
-        //         // if there is head node
-        //         // if (this.head != null)
-        //         // {
-        //         //     this.tail.Next = copy;
-        //         // }
-        //         // // set new head if none
-        //         // else
-        //         // {
-        //         //     this.head = copy;
-        //         // }
-        //         // this.tail = copy;
-
-        //         // add to dictionary
-
-        //     }
-        // }
-
         // this should be a to string thang
         // just prints out the vals of the linked list
         public static void forEach(MyLinkedList list)
@@ -192,8 +150,6 @@ namespace ConsoleApplication
             // DRY UP!
             for (int i = 0; i < listLength; i += 2)
             {
-                // Node copy1, copy2 = MyLinkedList.Copies(currNode);
-
                 Node copy1 = new Node();
                 Node copy2 = new Node();
 
